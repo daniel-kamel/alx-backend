f4b8b0896491b54c5c3bde48e7b3f4097c7da355
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2025-05-20 23:11:10 +0300

    Update app and template titles for consistency

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""A Basic Flask app.
+"""
+A Basic Flask app.
 """
 from flask import Flask, render_template
 
@@ -10,7 +11,8 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def get_index() -> str:
-    """The home/index page.
+    """
+    The home/index page.
     """
     return render_template('0-index.html')
 
